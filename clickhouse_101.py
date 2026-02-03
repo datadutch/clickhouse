4ac538b5ac85667dacb56e333d9ebe88755ef024
Johan van der Kooij <johan.van.der.kooij@alliander.com>
2026-02-03 16:52:29 +0100

    sec fix

--- clickhouse_101.py
+++
@@ -1,3 +0,0 @@
-import clickhouse_connect
-
-client = clickhouse_connect.get_client(host='838db9b4-0fe4-4b59-a62d-0da24915ce11.dtwh.fr-par.scw.cloud', port=8443, username='scwadmin', password='Clash9-Manicure8-Slang0')
